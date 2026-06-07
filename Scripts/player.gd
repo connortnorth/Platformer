de0858db559655a52243cffd9292ab7e5f9c6a9b
Breadboy9439 <connortnorth@gmail.com>
2026-06-07 14:21:56 -0500

    initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -850
+
+func _physics_process(delta: float) -> void:
+
+	if velocity.x < -1 or velocity.x > 1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animated_sprite_2d.animation = "jumping"
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	if direction == -1.0: 
+		animated_sprite_2d.flip_h = true
