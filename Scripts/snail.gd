4b39e6ac1b6fbc4035ca494fb903ad275ceb832e
Breadboy9439 <connortnorth@gmail.com>
2026-06-07 16:16:36 -0500

    I added enemies

---
+++ Scripts/snail.gd
@@ -0,0 +1,27 @@
+extends Area2D
+@onready var tile_map_layer: TileMapLayer = $"../../TileMapLayer"
+@onready var snail_animate: AnimatedSprite2D = $"Snail Animate"
+
+
+const SPEED = 100
+var direction = -1
+var overlapping = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if overlaps_body(tile_map_layer):
+		if overlapping == 0:
+			direction *= -1
+			overlapping = 1
+	else:
+		overlapping = 0
+	position.x += direction * SPEED * delta
+	
+	if direction == -1:
+		snail_animate.flip_h = false
+	else:
+		snail_animate.flip_h = true
