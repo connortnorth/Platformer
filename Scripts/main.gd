99c45af0798305eb9bd9643ec0460cf3a2883ab7
Breadboy9439 <connortnorth@gmail.com>
2026-06-09 18:50:09 -0500

    I added a fade to and from black

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,25 +1,31 @@
 extends Node2D
 
+@onready var fade: ColorRect = $HUD/Fade
+
 var current_level_root: Node = null
 var level = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#setup the level
+	fade.modulate.a = 1.0
 	current_level_root = get_node("LevelRoot")
 	_load_level(level)
-
 
 func _load_level(level_number: int) -> void:
 	if current_level_root:
 		current_level_root.queue_free()
-		
+
 	var level_path = "res://Scenes/Levels/level%s.tscn" % level_number
 	current_level_root = load(level_path).instantiate()
 	add_child(current_level_root)
 	current_level_root.name = "LevelRoot"
 	_setup_level(current_level_root)
-	
+
+	#fade in:
+	_fade(0.0)
+
+
 func _setup_level(level_root: Node) -> void:
 	#Connect Level
 	var Exit = level_root.get_node("Exit")
@@ -56,3 +62,10 @@ var score = 0
 func _apple_collected():
 	score += 1
 	score_label.text = "Score : %s" % score
+
+#fade:
+
+func _fade(to_alpha: float) -> void:
+	var tween := create_tween()
+	tween.tween_property(fade, "modulate:a", to_alpha, 1.5)
+	await tween.finished
